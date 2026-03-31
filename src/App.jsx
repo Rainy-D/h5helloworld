3565c78c6a701bf856e589ee37d809edb51e1a84
sea <sea@users.noreply.github.com>
2026-03-31 13:31:00 +0800

    init: react+vite h5 hello world

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <section id="center">
+      <h1>Hello World</h1>
+      <p>这是一个使用 React + Vite 构建的 H5 页面</p>
+    </section>
+  )
+}
+
+export default App
